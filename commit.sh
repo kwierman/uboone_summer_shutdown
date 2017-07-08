4909b9a97f44c79fcce2c5ecfc4653e92a14c3c0
Kevin Wierman <kwierman@gmail.com>
2017-07-07 19:55:36 -0500

    Adding new files

---
+++ commit.sh
@@ -0,0 +1,9 @@
+cp microboone_summer17.png index.png
+cp microboone_summer17.res.png index.res.png
+cp microboone_summer17.html index.html
+cp microboone_summer17-tasks.html index-tasks.html
+cp microboone_summer17-resources.html index-resources.html
+cp microboone_summer17-chart.html index-chart.html
+
+git commit -am "Update"
+git push
